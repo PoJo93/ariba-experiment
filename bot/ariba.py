588418580bc99c6145089b0e7459b3136ec809ab
Jonas Pohlmann <jonaspohlmann@yahoo.de>
2019-01-30 10:53:01 -0800

    Calling a GET API

--- bot/ariba.py
+++ bot/ariba.py
@@ -8,9 +8,7 @@ class AribaClient:
     """This client handles the calls to the Ariba API enpoints"""
 
     def _call_api(self, cai_conversation, api):
-        payload = api.build_api_request(self, conversation=cai_conversation)
-        print(payload)
-        return requests.post(api.address, json=payload)
+        return requests.get(api.address, headers=api.headers)
 
     def call_pending_approvables_api(self, cai_conversation):
         return self._call_api(cai_conversation=cai_conversation, api=PendingApprovablesAPI)
@@ -19,6 +17,7 @@ class AribaClient:
 
 class AribaApiType:
     address = None
+    headers = {}
 
     def build_api_request(self, conversation: CAIConversation):
         pass
@@ -26,7 +25,7 @@ class AribaApiType:
 
 class PendingApprovablesAPI(AribaApiType):
     address = 'https://itg-openapi.aws.ariba.cm:8443/api/approval-copilot/v1/sandbox/changes?realm=mytestrealm'
-    headers = ["accept: application/json", "apiKey: 5bb8b7009a294f408fd01a263a3bcd68"]
+    headers = {"accept": "application/json", "apiKey": "5bb8b7009a294f408fd01a263a3bcd68"}
 
     def build_api_request(self, conversation: CAIConversation):
         return ""
