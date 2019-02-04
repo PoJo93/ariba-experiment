4d7ec7c6602df4d6603d9fff549dff53695f031b
Jonas Pohlmann <jonaspohlmann@yahoo.de>
2019-02-04 15:44:42 -0800

    Refactored method process_request to customize api endpoints

--- server.py
+++ server.py
@@ -12,8 +12,24 @@ ariba_client = ariba.AribaClient()
 
 @app.route("/approvables/show", methods=['POST'])
 def show_approvables():
-    return process_request(request, ariba_client.call_pending_approvables_api)
+    cai_json = json.loads(request.get_data().decode('utf-8'))
+    cai_conversation = cai.CAIConversation.from_json_payload(cai_json)
 
+    response = ariba_client.call_pending_approvables_api(cai_conversation)
+
+    messages = ariba_client.process_ariba_response_approvables(response)
+    return cai.build_response(cai_conversation, response, messages)
+
+
+@app.route("/approvables/check", methods=['POST'])
+def check_approvables():
+    cai_json = json.loads(request.get_data().decode('utf-8'))
+    cai_conversation = cai.CAIConversation.from_json_payload(cai_json)
+
+    response = ariba_client.call_pending_approvables_api(cai_conversation)
+
+    messages = ''
+    return cai.build_response(cai_conversation, response, messages)
 
 
 @app.route("/dialogflow/test", methods=['POST'])
@@ -21,14 +37,7 @@ def debug_dialogflow():
     request_json =json.loads(request.get_data().decode('utf-8'))
     print(json.dumps(request_json, indent=5, sort_keys=False))
 
-def process_request(request_cai, clientApiMethod):
-    cai_json = json.loads(request_cai.get_data().decode('utf-8'))
-    cai_conversation = cai.CAIConversation.from_json_payload(cai_json)
 
-    response = clientApiMethod(cai_conversation)
-    messages = ariba_client.process_ariba_response_approvables(response)
-
-    return cai.build_response(cai_conversation, response, messages)
 
 
 if __name__ == '__main__':
